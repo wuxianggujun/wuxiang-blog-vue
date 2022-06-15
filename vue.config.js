20a00f899f2e840a54e0ffbc12d7b06c060a5109
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-15 08:02:52 +0800

    新建vue框架

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        //配置前端项目的访问端口
+        port: 9999
+    }
 })
